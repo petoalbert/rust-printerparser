391bd3696e85b27395fe5b569e7fbbb51be57baf
bkrmendy <korber314@gmail.com>
2023-05-21 15:12:47 +0200

    format main

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 mod printerparser;
 
-use printerparser::*;
 use crate::printerparser::PrinterParser;
+use printerparser::*;
 
 #[derive(Debug, Clone)]
-struct FunDef { name: String, args: Vec<String> }
+struct FunDef {
+    name: String,
+    args: Vec<String>,
+}
 
 fn main() {
-    let name = as_string(ANY_CHAR.filter(|c| *c != '(' && *c != ',' && *c != ')').repeat());
-    let args = surrounded_by(string("("), separated_list(name.clone(), string(",")), string(")"));
-    let fun = preceded_by(string("fn "), name).zip_with(args).map(
-        |(name, args)| FunDef { name: name, args: args.into_iter().collect() },
-        |f| (f.name.clone(), f.args.clone().into_iter().collect())
+    let name = as_string(
+        ANY_CHAR
+            .filter(|c| *c != '(' && *c != ',' && *c != ')')
+            .repeat(),
     );
-    
-    let parsed = fun.parse("fn test(a,b,c)",&mut ()).unwrap().1;
+    let args = surrounded_by(
+        string("("),
+        separated_list(name.clone(), string(",")),
+        string(")"),
+    );
+    let fun = preceded_by(string("fn "), name).zip_with(args).map(
+        |(name, args)| FunDef {
+            name: name,
+            args: args.into_iter().collect(),
+        },
+        |f| (f.name.clone(), f.args.clone().into_iter().collect()),
+    );
+
+    let parsed = fun.parse("fn test(a,b,c)", &mut ()).unwrap().1;
     println!("Parsed: {:#?}", parsed);
     let printed = fun.print(parsed, &mut ()).unwrap();
     println!("Printed: {}", printed);
 }
-
-
