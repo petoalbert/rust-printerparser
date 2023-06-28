97061f581c0a8aaf09e8f01ab8154fe531776187
bkrmendy <korber314@gmail.com>
2023-06-28 21:31:05 +0200

    test for init command

--- src/commands/init_command.rs
+++ src/commands/init_command.rs
@@ -9,3 +9,30 @@ pub fn run_init_command(db_path: &str) {
     db.write_current_latest_commit("initial")
         .expect("Cannot write current latest commit");
 }
+
+#[cfg(test)]
+mod test {
+    use tempfile::TempDir;
+
+    use crate::db_ops::{Persistence, DB};
+
+    use super::run_init_command;
+
+    #[test]
+    fn test_post_init_state() {
+        let tmp_dir = TempDir::new().expect("Cannot create temp dir");
+        let tmp_path = tmp_dir.path().to_str().expect("Cannot get temp dir path");
+        run_init_command(tmp_path);
+
+        let db = Persistence::open(tmp_path).expect("Cannot open db");
+        let current_branch_name = db
+            .read_current_branch_name()
+            .expect("Cannot read current branch name");
+        assert_eq!(current_branch_name, "main");
+
+        let latest_commit_name = db
+            .read_current_latest_commit()
+            .expect("Cannot read latest commit");
+        assert_eq!(latest_commit_name, "initial");
+    }
+}
