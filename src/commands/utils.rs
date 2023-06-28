5abbd64e0e7935bff087b500a0206ab0080c8b3c
bkrmendy <korber314@gmail.com>
2023-06-28 20:16:15 +0200

    timer macro

--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -19,6 +19,25 @@ pub fn hash_list() -> impl PrinterParserOps<(), Vec<String>> {
     separated_list(hex_string(), char(','))
 }
 
+#[macro_export]
+macro_rules! measure_time {
+    ($name:expr, $block:expr) => {{
+        #[cfg(debug_assertions)]
+        {
+            println!("{}...", $name);
+            let start = std::time::Instant::now();
+            let result = $block;
+            let duration = start.elapsed();
+            println!("{} took: {:?}", $name, duration);
+            result
+        }
+        #[cfg(not(debug_assertions))]
+        {
+            $block
+        }
+    }};
+}
+
 #[cfg(test)]
 mod test {
     use crate::printer_parser::printerparser::PrinterParser;
