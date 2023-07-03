64700194a2f8b8671d98da40e41d953ea30b437a
bkrmendy <korber314@gmail.com>
2023-07-03 09:27:14 +0200

    tauri

--- crates/parserprinter/src/lib.rs
+++ crates/parserprinter/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod blend;
 pub mod commands;
-pub mod printer_parser;
 pub mod db_ops;
+pub mod printer_parser;
+
+pub mod test;
---
+++ crates/parserprinter/src/test.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+pub fn cwd() -> String {
+    let res = env::current_dir();
+    match res {
+        Ok(path) => path.into_os_string().into_string().unwrap(),
+        Err(_) => "FAILED".to_string()
+    }
+}
