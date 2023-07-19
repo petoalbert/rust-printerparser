5c8eb597030ee734a99b6e7600fa51487be68731
bkrmendy <korber314@gmail.com>
2023-07-19 21:19:46 +0200

    list checkpoints

--- crates/desktop/src-plugin/addon.py
+++ crates/desktop/src-plugin/addon.py
@@ -1,7 +1,9 @@
 import bpy
-import time
 from threading import Timer
 import requests
+from urllib.parse import quote_plus
+import json
+import os
 
 bl_info = {
     "name": "Timeline Tool",
@@ -9,41 +11,79 @@ bl_info = {
     "category": "Object",
 }
 
+def get_file_path():
+    return bpy.data.filepath
+
+def get_db_path(file_path):
+    return os.path.join(os.path.dirname(file_path), ".timeline")
+
+def call_commit_api(message):
+    file_path = get_file_path()
+    db_path = get_db_path(file_path)
+    url = 'http://127.0.0.1:8080/commit'
+    headers = {'Content-Type': 'application/json'} 
+    data = {
+        'db_path': db_path,
+        'file_path': file_path,
+        'message': message,
+    }
+
+    return requests.post(url, headers=headers, data=json.dumps(data))
+
+def call_checkpoints_api():
+    file_path = get_file_path()
+    db_path = get_db_path(file_path)
+    url = f'http://127.0.0.1:8080/checkpoints/{quote_plus(db_path)}'
+
+    response = requests.get(url)
+    return response.json()
+
+
 def onload_operator():
     bpy.ops.wm.onload_operator()
 
 class OnloadOperator(bpy.types.Operator):
-    bl_idname = "wm.test_operator"
+    bl_idname = "wm.onload_operator"
     bl_label = "Test Operator"
+    
     def execute(self, context):
-        for i in range(5):
-            item = bpy.context.scene.my_items.add()
-            item.name = f"Item {i+1}"
+        self.report({'INFO'}, "Calling checkpoints api...")
+        items = call_checkpoints_api()
+        self.report({'INFO'}, f"{items}")
+        
+        bpy.context.scene.checkpoint_items.clear()
+
+        for i in items:
+            item = bpy.context.scene.checkpoint_items.add()
+            item.hash = i['hash']
+            item.message = i['message']
     
         return {'FINISHED'}
 
-class MY_PG_Item(bpy.types.PropertyGroup):
-    name: bpy.props.StringProperty(name="Item Name", default="Item")
+class CommitItem(bpy.types.PropertyGroup):
+    message: bpy.props.StringProperty(name="Message", default="")
+    hash: bpy.props.StringProperty(name="Hash", default="")
 
-class MY_OT_UpdateItem(bpy.types.Operator):
-    bl_idname = "my.update_item"
+class CheckoutItemOperator(bpy.types.Operator):
+    bl_idname = "my.checkout_item"
     bl_label = "Update Item"
-    item_index: bpy.props.IntProperty()
+    hash: bpy.props.StringProperty(name="Hash", default="")
 
     def execute(self, context):
-        context.scene.my_items[self.item_index].name = context.scene.my_string_prop
+        self.report({'INFO'}, f"{self.hash}")
         return {'FINISHED'}
 
-class MY_OT_Operator(bpy.types.Operator):
-    bl_idname = "my.operator"
-    bl_label = "My Operator"
+class CommitOperator(bpy.types.Operator):
+    bl_idname = "my.commit"
+    bl_label = "Commit Operator"
 
     def execute(self, context):
-        resp = requests.get('http://127.0.0.1:8080')
-        self.report({'INFO'}, resp.text)
+        bpy.ops.wm.save_mainfile()
+        
+        message = context.scene.commit_message
+        response = call_commit_api(message)
+        self.report({'INFO'}, f"{response.status_code}")
         return {'FINISHED'}
-    
-
 
 class TimelinePanel(bpy.types.Panel):
     bl_label = "Timeline"
@@ -59,22 +99,22 @@ class TimelinePanel(bpy.types.Panel):
         layout.prop(context.scene, "my_enum_prop", text="")
 
         # List of items
-        for i, item in enumerate(context.scene.my_items):
+        for item in context.scene.checkpoint_items:
             row = layout.row()
-            row.label(text=item.name)
-            row.operator("my.update_item", text="Update").item_index = i
+            row.label(text=item.message)
+            row.operator("my.checkout_item", text="Restore").hash = item.hash
 
         # Text field
-        layout.prop(context.scene, "my_string_prop", text="")
+        layout.prop(context.scene, "commit_message", text="")
 
         # Blue button at the bottom
         row = layout.row()
-        row.operator("my.operator", text="Blue Button")
+        row.operator("my.commit", text="Create Checkpoint")
 
 def register():
-    bpy.utils.register_class(MY_PG_Item)
-    bpy.utils.register_class(MY_OT_UpdateItem)
-    bpy.utils.register_class(MY_OT_Operator)
+    bpy.utils.register_class(CommitItem)
+    bpy.utils.register_class(CheckoutItemOperator)
+    bpy.utils.register_class(CommitOperator)
     bpy.utils.register_class(TimelinePanel)
     bpy.utils.register_class(OnloadOperator)
     bpy.types.Scene.my_enum_prop = bpy.props.EnumProperty(
@@ -85,20 +125,20 @@ def register():
             ("OPTION4", "Option 4", ""),
         ]
     )
-    bpy.types.Scene.my_string_prop = bpy.props.StringProperty(name="")
-    bpy.types.Scene.my_items = bpy.props.CollectionProperty(type=MY_PG_Item)
+    bpy.types.Scene.commit_message = bpy.props.StringProperty(name="")
+    bpy.types.Scene.checkpoint_items = bpy.props.CollectionProperty(type=CommitItem)
 
     Timer(1, onload_operator, ()).start()
 
 def unregister():
     bpy.utils.unregister_class(TimelinePanel)
-    bpy.utils.unregister_class(MY_PG_Item)
-    bpy.utils.unregister_class(MY_OT_UpdateItem)
-    bpy.utils.unregister_class(MY_OT_Operator)
+    bpy.utils.unregister_class(CommitItem)
+    bpy.utils.unregister_class(CheckoutItemOperator)
+    bpy.utils.unregister_class(CommitOperator)
     bpy.utils.unregister_class(OnloadOperator)
-    del bpy.types.Scene.my_items
+    del bpy.types.Scene.checkpoint_items
     del bpy.types.Scene.my_enum_prop
-    del bpy.types.Scene.my_string_prop
+    del bpy.types.Scene.commit_message
 
 if __name__ == "__main__":
     register()
