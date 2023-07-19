da43e4b24c526f8455f6923380e36687f33ee568
bkrmendy <korber314@gmail.com>
2023-07-19 21:56:02 +0200

    as above

--- crates/desktop/src-tauri/src/server/endpoints.rs
+++ crates/desktop/src-tauri/src/server/endpoints.rs
@@ -1,6 +1,14 @@
-use actix_web::{get, post, web::Json, HttpResponse, Responder};
-use parserprinter::api::commit_command::create_new_commit;
-use serde::Deserialize;
+use actix_web::{
+    get, post,
+    web::{self, Json},
+    HttpResponse, Responder,
+};
+use parserprinter::api::{
+    commit_command::create_new_commit,
+    log_checkpoints_command::log_checkpoints,
+    restore_command::{self, restore_checkpoint},
+};
+use serde::{Deserialize, Serialize};
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
@@ -21,6 +29,42 @@ pub async fn commit(data: Json<CommitPayload>) -> impl Responder {
         &data.db_path,
         Some(data.message.to_owned()),
     );
+
+    match result {
+        Err(_) => HttpResponse::BadRequest(),
+        Ok(_) => HttpResponse::Ok(),
+    }
+}
+
+#[derive(Serialize)]
+struct ShortCommitPayload {
+    hash: String,
+    message: String,
+}
+
+#[get("/checkpoints/{db_path}")]
+pub async fn checkpoints(path: web::Path<(String,)>) -> impl Responder {
+    let checkpoints: Vec<ShortCommitPayload> = log_checkpoints(&path.0.to_owned(), None)
+        .into_iter()
+        .map(|checkpoint| ShortCommitPayload {
+            hash: checkpoint.hash,
+            message: checkpoint.message,
+        })
+        .collect();
+
+    HttpResponse::Ok().json(checkpoints)
+}
+
+#[derive(Deserialize)]
+pub struct RestorePayload {
+    db_path: String,
+    file_path: String,
+    hash: String,
+}
+
+#[post("/restore")]
+pub async fn restore(data: Json<RestorePayload>) -> impl Responder {
+    let result = restore_checkpoint(&data.file_path, &data.db_path, &data.hash);
 
     match result {
         Err(_) => HttpResponse::BadRequest(),
--- crates/desktop/src-tauri/src/server/serve.rs
+++ crates/desktop/src-tauri/src/server/serve.rs
@@ -1,12 +1,18 @@
 use actix_web::{App, HttpServer};
 
-use super::endpoints::{commit, hello};
+use super::endpoints::{checkpoints, commit, hello, restore};
 
 pub async fn serve() {
-    HttpServer::new(|| App::new().service(hello).service(commit))
-        .bind(("127.0.0.1", 8080))
-        .expect("Cannot bind to 127.0.0.1:8080")
-        .run()
-        .await
-        .unwrap()
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(commit)
+            .service(checkpoints)
+            .service(restore)
+    })
+    .bind(("127.0.0.1", 8080))
+    .expect("Cannot bind to 127.0.0.1:8080")
+    .run()
+    .await
+    .unwrap()
 }
